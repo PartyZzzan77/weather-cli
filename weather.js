f72b2d2e2c1d389309f1a3fa55c08d899e8e3916
Mitya <partyzan327@gmail.com>
2022-12-15 15:17:11 +0300

    feat: add loggers

--- weather.js
+++ weather.js
@@ -1,13 +1,15 @@
 #!usr/bin/env node
 
 import { getArgs } from './helpers/getArgs.js';
+import { printHelp } from './services/log.services.js';
 
 const initCLI = () => {
-  const args = getArgs(process.argv.slice(2));
-
+  const args = getArgs(process.argv);
+  console.log(args);
   if (args.s) {
   }
   if (args.h) {
+    printHelp();
   }
   if (args.t) {
   }
