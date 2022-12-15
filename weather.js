c56d380dd39005f765e8a35740587f7e5ef4a866
Mitya <partyzan327@gmail.com>
2022-12-15 15:02:41 +0300

    feat: implement the env variable retrieval

--- weather.js
+++ weather.js
@@ -1,7 +1,16 @@
 #!usr/bin/env node
 
+import { getArgs } from './helpers/getArgs.js';
+
 const initCLI = () => {
-  console.log('started');
+  const args = getArgs(process.argv.slice(2));
+
+  if (args.s) {
+  }
+  if (args.h) {
+  }
+  if (args.t) {
+  }
 };
 
 initCLI();
